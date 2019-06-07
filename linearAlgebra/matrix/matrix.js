cddfa66a69e27854abd1fcdb5bff5df024919026
bahaaEldeen <bahaaeldeen1999@gmail.com>
2019-06-07 20:01:14 +0200

    print

--- linearAlgebra/matrix/matrix.js
+++ linearAlgebra/matrix/matrix.js
@@ -5,8 +5,19 @@ class Matrix{
         this.rows = matrix.length;
         this.cols = matrix[0].length;
     }
-    gaussianElimenation(arr){
-        arr = [];
+    print(){
+        let s = ''
+        for(let i=0;i<this.rows;i++){
+           s += '| ';
+            for(let j=0;j<this.cols;j++){
+              s+= this.matrix[i][j]+' ';
+            }
+           s+= '| \n';
+        }
+        console.log(s)
+    }
+    gaussianElimenation(){
+        let arr = [];
         let x = 0;
         let j = 0;
        
@@ -15,9 +26,13 @@ class Matrix{
        
        
         for(let i=0;i<this.rows-1;i++){
+            let arr1 = [];
             for(let l = i+1;l<this.rows;l++){
                 x = -1*copyMatrix[l][j]/copyMatrix[i][j];
-               // arr.push(x);
+                for(let n=0;n<i;n++){
+                    arr1.push(0)
+                }
+                arr1.push(x);
                 let a = [];
                 for(let k=0;k<this.cols;k++){
                     a.push(x*copyMatrix[i][k]);
@@ -29,22 +44,26 @@ class Matrix{
 
                }
             }
-            // for(let k=0;k<this.cols;k++){
-            //     let newElement = a[k] + copyMatrix[i+1][k];
-            //     copyMatrix[i+1][k] = newElement;
-
-            // }
+            arr.push(arr1);
+           
              j++;
+            
         }
-        return copyMatrix;
+        return {
+            'guassianMatrix': new Matrix(copyMatrix),
+            'operationsArray': arr
+        };
     }
     
 }
 
 let h = new Matrix([
-    [1,2,3],
-    [4,5,6],
-    [7,8,19]
+    [1,2,3,8],
+    [4,5,6,67],
+    [7,8,19,6],
+    [7,8,19,6],
 ]);
-let g = []
-console.log(h.gaussianElimenation(g))
+h.print()
+let g = h.gaussianElimenation()
+
+//console.log(g)
